5966bd589e56d14d6f487c26cfa0fbe645cf91f4
Aki Ito <a-ito@o-uccino.jp>
2019-11-07 16:17:56 +0900

    Footerを追加する

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,19 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: Header(),
-        body: Center(child: Text("オラオラオラ"),)
+        body: Center(child: Text("オラオラオラ")),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+          ],
+        ),
       ),
     );
   }
